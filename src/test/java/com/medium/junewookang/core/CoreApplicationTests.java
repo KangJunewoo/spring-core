88cf7bc62a79891bff29b6e4d8c6a20735adc39b
KangJunewoo <brianjune.dev@gmail.com>
2021-06-11 16:31:32 +0900

    feat: 스프링 원리 프로젝트 시작

---
+++ src/test/java/com/medium/junewookang/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.junewookang.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
