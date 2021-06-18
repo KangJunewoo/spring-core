11e81178081fc1679bd962f797fa38081019763e
KangJunewoo <brianjune.dev@gmail.com>
2021-06-18 16:08:34 +0900

    refactor: AppConfig 리팩터링

--- src/main/java/com/medium/junewookang/core/AppConfig.java
+++ src/main/java/com/medium/junewookang/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.medium.junewookang.core;
 
+import com.medium.junewookang.core.discount.DiscountPolicy;
 import com.medium.junewookang.core.discount.FixDiscountPolicy;
+import com.medium.junewookang.core.member.MemberRepository;
 import com.medium.junewookang.core.member.MemberService;
 import com.medium.junewookang.core.member.MemberServiceImpl;
 import com.medium.junewookang.core.member.MemoryMemberRepository;
@@ -12,10 +14,18 @@ public class AppConfig {
     // 이걸 memberService 혹은 orderService에 대한 DI라고 함. 의존관계 주입 혹은 의존성 주입.
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
